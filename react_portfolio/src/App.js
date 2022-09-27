f8ba31d6f208e2b61c4e507cb932d994fcba271b
Mukesh Raj <mukesh@techis.io>
2022-09-28 03:17:47 +0530

    Added my files

---
+++ react_portfolio/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function App() {
+  const User=()=>{
+    return(
+      <div>
+        <h1>Mukesh</h1>
+        <h1>25</h1>
+        <h1>Mukesh@techis.io</h1>
+      </div>
+
+    )
+  }
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
